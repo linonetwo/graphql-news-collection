96d8ddf0d76baab6d432e7982a60c63d1994dd66
linonetwo <linonetwo012@gmail.com>
2017-06-27 12:41:11 +0800

    feat: error handling

--- src/client.js
+++ src/client.js
@@ -1,20 +1,26 @@
 /* @flowtype @ts-check */
 import 'whatwg-fetch';
 import fetch from 'node-fetch';
+import ApolloClient from 'apollo-client';
+
 import getGraphqlClient from './client/networkInterface';
-import Seeds from './client/Seeds';
-import Crawl from './client/Crawl';
-import Stories from './client/Stories';
+import Seeds from './client/sagas/Seeds';
+import Crawl from './client/sagas/Crawl';
+import Stories from './client/sagas/Stories';
 
 global.fetch = fetch;
 
 async function runCrawler() {
-  const client = await getGraphqlClient();
+  const client: ApolloClient = await getGraphqlClient()
+    .catch((error) => {
+      console.error(`Crawler init error: ${error}`);
+      process.exit(0);
+    });
 
   // let tasks start
-  Seeds(client);
-  Crawl();
-  Stories(client);
+  Seeds(client).catch(error => console.error(`Fatal error in Seed() : ${error}`));
+  Crawl().catch(error => console.error(`Fatal error in Crawl() : ${error}`));
+  Stories(client).catch(error => console.error(`Fatal error in Stories() : ${error}`));
 }
 
 runCrawler();
