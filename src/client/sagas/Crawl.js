8b3d55123f38b7bd9654a0446f58b6c617f503ea
linonetwo <linonetwo012@gmail.com>
2017-07-10 16:38:59 +0800

    feat: add headless-chrome

--- src/client/sagas/Crawl.js
+++ src/client/sagas/Crawl.js
@@ -1,10 +1,22 @@
 /* @flow @ts-check */
+import HeadlessChrome from 'simple-headless-chrome';
 import { seeds, stories } from '../channels';
 
 import type { Story } from '../types';
 
 async function getStory(url: string): Story {
-  console.log(await fetch(url));
+  const browser = new HeadlessChrome({
+    headless: true,
+    launchChrome: false,
+    chrome: {
+      host: 'localhost',
+      port: 9222,
+      remote: true
+    },
+    browserlog: true
+  });
+  await browser.init();
+  await browser.goTo(url);
   return { title: '请问人情味儿', text: '阿萨德发生大幅', url: 'http://fakeurl111.com', clientMutationId: 'qwerasdf' };
 }
 
